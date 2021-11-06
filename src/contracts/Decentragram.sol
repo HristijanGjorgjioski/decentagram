6a897e4b61fbc81386533ea45a9a25721586af98
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-06 03:16:23 -0400

    Creating my own data structure Image and creating functionfor uploadImages

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract Decentragram {
   string public name = "Decentragram";
+
+  //* Store Images
+  mapping(uint => Image) public images;
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    unit tipAmount;
+    address payable author;
+  }
+
+  //* Create Images
+  function uploadImage() public {
+    
+  }
+
+  //* Tip Images
 }
