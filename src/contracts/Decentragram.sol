2e9a349e23599cd70b7f5c928ef42c39c3b1e01f
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-06 03:28:46 -0400

    Creating dynamic counter for image id and dynamic variables for each image

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -4,18 +4,25 @@ contract Decentragram {
   string public name = "Decentragram";
 
   //* Store Images
+  uint public imageCount = 0;
+
   mapping(uint => Image) public images;
+
   struct Image {
     uint id;
     string hash;
     string description;
-    unit tipAmount;
+    uint tipAmount;
     address payable author;
   }
 
   //* Create Images
-  function uploadImage() public {
-    
+  function uploadImage(string memory _imgHash, string memory _description) public {
+    //* Increment image id
+    imageCount++;
+
+    //* Add image to contract
+    images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
   }
 
   //* Tip Images
